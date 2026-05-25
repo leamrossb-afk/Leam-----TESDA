b606a4e83fcfd1f521b5b2b0832c5af26be0e12f
leamrossb-afk <leamrossb@gmail.com>
2026-05-25 12:13:50 +0000

    Day 4 - Commit from GitHub

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
 class Main{
     public static void main(String[] args) {
-        System.out.println("Hello World"); // Codespace
-        System.out.println("Hello World2"); // VS Code local PC
+        System.out.println("Hello World"); // From codespace
+        /* Commit & sync + commit message
+        Open GitHub for desktop then open the project in VS Code */
+        System.out.println("Hello World2"); // From VS Code local PC
+        /* Commit & sync + commit message
+        * On GitHub codespace, click "Pull" to pull the changes from PC 
+        * Check for similar files, pull will abort if the destination has
+            the same file with the files being pulled */
+        System.out.println("Hello World3"); // From codespace again
     }
 }
