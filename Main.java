64d4e4e31d292de17edb522cecf27566d5e41f10
leamrossb-afk <leamrossb@gmail.com>
2026-05-30 00:33:23 +0800

    Task2

--- Main.java
+++ Main.java
@@ -1,13 +1,17 @@
-class Main{
+public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello World"); // From codespace
-        /* Commit & sync + commit message
-        Open GitHub for desktop then open the project in VS Code */
+        System.out.println("Hello World"); // From codespace.
         System.out.println("Hello World2"); // From VS Code local PC
-        /* Commit & sync + commit message
-        * On GitHub codespace, click "Pull" to pull the changes from PC 
-        * Check for similar files, pull will abort if the destination has
-            the same file with the files being pulled */
-        System.out.println("Hello World3"); // From codespace again
+        System.out.println("Hello World3"); // From codespace again.
     }
+}
+class Test {
+    public static void main(String[] args) {
+        char h = 'H', w = 'w', r = 'r', d = 'd';
+        int ello = 3110;
+        byte o = 0;
+        short l = 1;
+        String output = "" + h + ello + " " + w + o + r + l + d;
+        System.out.println(output);
+}
 }
