075b06503a4f76ffca62f58cab24faecd3bebc0d
leamrossb-afk <leamrossb@gmail.com>
2026-05-29 16:40:10 +0000

    Task2(2.1)

--- Main.java
+++ Main.java
@@ -11,7 +11,9 @@ class Test {
         int ello = 3110;
         byte o = 0;
         short l = 1;
-        String output = "" + h + ello + " " + w + o + r + l + d;
+        float f = 2.0f;
+        boolean b = true;
+        String output = "" + h + ello + " " + w + o + r + l + d + " " + f + " " + b; 
         System.out.println(output);
 }
 }
